673b1dbe3659a83c1289d6ef503b73efa09994d2
kcprlk <50843010+kcprlk@users.noreply.github.com>
2021-01-06 23:10:25 +0100

    Update Lab7zad1.c

--- Lab7zad1.c
+++ Lab7zad1.c
@@ -1,1 +1,30 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void *Hello(void *threadID)
+{
+  printf("Hello SCR. Written by thread %ld\n", threadID);
+  pthread_exit(NULL);
+}
+
+int main()
+{
+  int numOfThreads = 10;
+  pthread_t threads[numOfThreads];
+  int err;
+
+  
+  for( int i = 0; i < numOfThreads; i++)
+    {
+      err = pthread_create(&threads[i], NULL, Hello, (void *)i);
+      if (err)
+	{
+	  printf("ERROR, from %d\n", err);
+	  exit(-1);
+	}
+
+    }
+  pthread_exit(NULL);
+}
 
